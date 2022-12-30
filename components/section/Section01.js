80b76dfc50a1ec285f0af7e9bd41c3f894568c45
jt01052552295 <jt01052552295@gmail.com>
2022-12-30 19:35:31 +0900

    section1

--- components/section/Section01.js
+++ components/section/Section01.js
@@ -4,7 +4,21 @@ import PropTypes from 'prop-types';
 const Section01 = (props) => {
   return (
     <section className="st st01">
-      <div className="fd-container-fluid">css animation</div>
+      <div className="ani-container">
+        <article className="sky">
+          <img src="/images/section01/sun.png" className="sun" />
+          <img src="/images/section01/cloud1.png" className="cloud1" />
+          <img src="/images/section01/cloud2.png" className="cloud2" />
+        </article>
+        <article className="town">
+          <img src="/images/section01/town_day.png" className="day" />
+          <img src="/images/section01/town_day.png" className="day2" />
+          <img src="/images/section01/circle.png" className="circle" />
+        </article>
+        <article className="people">
+          <img src="/images/section01/man.png" className="man" />
+        </article>
+      </div>
     </section>
   );
 };
